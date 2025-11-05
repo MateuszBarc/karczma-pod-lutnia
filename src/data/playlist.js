b8e7f3a8ce9ef8a5aad4dbcd29ef039b54d0db25
Mateusz Barc <mateuszbarc1996@gmail.com>
2025-11-05 16:59:32 +0100

    Add Data file playlist

---
+++ src/data/playlist.js
@@ -0,0 +1,60 @@
+export const playlist = [
+    {
+        id: 1,
+        name: "Ballady o przyjaciołach",
+        description: "Zbiór ballad opowiadających o niezwykłych przygodach i przyjaźniach.",
+        songs: [
+            {
+            id: "bp1", 
+            title: "Ballada o Kapeluszu co nie chcial juz spiewac", 
+            artist: "Bard z Hogwartu", 
+            audio: "../public/audio/balladaKapeluszu.mp3"
+            },
+            {
+            id: "bp2", 
+            title: "Ballada Chatka wśród drzew (Hymn Zwiadowców)", 
+            artist: "Will Treaty", 
+            audio: "../public/audio/balladaChatka.mp3"
+            },
+        ],
+    },
+      {
+        id: 2,
+        name: "Ballady o wiedzminach",
+        description: "Zbior ballad z swiata Wiedzmina.",
+        songs: [
+            {
+            id: "bw1", 
+            title: "Ballada o Truciznie i Cebuli", 
+            artist: "Bard Jaskier", 
+            audio: "../public/audio/balladaCebula.mp3"
+            },
+            {
+            id: "bw2", 
+            title: "Braterstwo Wilków", 
+            artist: "Bard z Khearn Moren", 
+            audio: "../public/audio/balladaWilkow.mp3"
+            },
+        ],
+    },
+     {
+        id: 2,
+        name: "Ballady o bogach greckich",
+        description: "Zbior ballad z swiata Wiedzmina.",
+        songs: [
+            {
+            id: "bbg1", 
+            title: "Ballada o Zeusie i Laptopie", 
+            artist: "Bard Grecki", 
+            audio: "../public/audio/balladaZeus.mp3"
+            },
+            {
+            id: "bbg2", 
+            title: "Ballada o Boskiej Biurokracji", 
+            artist: "Bard z Khearn Moren", 
+            audio: "../public/audio/balladaBiurokracja.mp3"
+            },
+        ],
+    },
+       
+]
